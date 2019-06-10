7ef6b44a9e742099d3ee484b27e2e95b8399547c
shiming <codingmylife@gmail.com>
2019-06-10 16:23:10 +0800

    feat: dead lock

---
+++ dead-lock/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main(){
+	type value struct {
+		mu sync.Mutex
+		value int
+	}
+
+	var wg sync.WaitGroup
+
+	printSum := func(v1, v2 *value) {
+		defer wg.Done()
+		v1.mu.Lock()
+		defer v1.mu.Unlock()
+		time.Sleep(2*time.Second)
+		v2.mu.Lock()
+		defer v2.mu.Unlock()
+		fmt.Printf("sum=%v\n", v1.value + v2.value)
+	}
+
+	var a, b value
+	wg.Add(2)
+	go printSum(&a, &b)
+	go printSum(&b, &a)
+	wg.Wait()
+}
+/*
+output:
+fatal error: all goroutines are asleep - deadlock!
+
+
+ */
